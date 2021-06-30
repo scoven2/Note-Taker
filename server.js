618bb09072f0e1b7237b1eecbc48430d9d6b9fdf
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-06-29 21:23:24 -0500

    changed quotation marks to apostrophe

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Dependencies
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 //unique id
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
 //Express app set up
 const app = express();
@@ -12,31 +12,31 @@ const PORT = process.env.PORT || 8888;
 
 //data parsing, encouding, static, middleware
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-//routes
+//routes 
 //index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 //api
-app.get("/api/notes", (req, res) => {
-    fs.readFile("./db.db.json", "utf8", (error, data) => {
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db.db.json', 'utf8', (error, data) => {
         console.log(data);
         res.send(data);
     })
 });
 
 //new note posted
-app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (error, data) => {
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
         console.log(data)
         const newNote = req.body
         newNote.id = uuidv4()
@@ -44,14 +44,14 @@ app.post("/api/notes", (req, res) => {
             //add new note
         notes.push(newNote)
             //save note array back to db file
-        fs.writeFile("./db/db.json", JSON.stringify(notes), () => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), () => {
             res.send(newNote);
         })
     })
 });
 
 //delete
-app.delete("/api/notes/:id", (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
     db.forEach((note) => {
         if (noteId === note.id) {
