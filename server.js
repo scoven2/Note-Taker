2301e27da6ea4a7848aff736083c024e0422461a
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-06-29 21:10:44 -0500

    corrected spelling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 const express = require("express");
 const path = require("path");
-const fs = reqiure("fs");
+const fs = require("fs");
 
 //unique id
 const { v4: uuidv4 } = require("uuid");
